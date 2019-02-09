42d63634909647115c8777b55f523b79c33cd843
Hearty Oh <heartyoh@hatiolab.com>
2019-02-10 01:34:28 +0900

    board-list thumbnail

--- src/pages/page-board-list.js
+++ src/pages/page-board-list.js
@@ -14,19 +14,61 @@ import { connect } from 'pwa-helpers/connect-mixin.js'
 import { store } from '../store.js'
 
 import { PageViewElement } from './page-view-element.js'
+import './page-toolbar'
+import '../components/search-input'
 
 class PageBoardList extends connect(store)(PageViewElement) {
+  constructor() {
+    super()
+
+    this.keyword = ''
+  }
+
   static get properties() {
     return {
-      boards: { type: Array }
+      boards: { type: Array },
+      keyword: { type: String }
     }
   }
 
   static get styles() {
     return [
       css`
+        :host {
+          display: flex;
+          flex-direction: column;
+
+          overflow: hidden;
+        }
+
+        nav {
+          flex: 1;
+
+          padding: 16px;
+          margin: 0px;
+          overflow-y: auto;
+
+          display: grid;
+
+          grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+          grid-auto-rows: minmax(200px, 220px);
+          grid-gap: 16px;
+        }
+
         a {
+          padding: 5px 5px 0 5px;
+          margin: 0;
+          height: 100%;
+          background-color: lightgray;
+          color: navy;
+          font-size: 0.8em;
+        }
+
+        img {
           display: block;
+          height: 88%;
+          max-width: 100%;
+          margin: auto;
         }
       `
     ]
@@ -34,11 +76,18 @@ class PageBoardList extends connect(store)(PageViewElement) {
 
   render() {
     return html`
+      <page-toolbar>
+        <search-input .value=${this.keyword} label="search"> </search-input>
+      </page-toolbar>
+
       <nav>
         ${this.boards.map(
           board =>
             html`
-              <a href="/viewer/${board.id}">${board.name}</a>
+              <a href="/viewer/${board.id}">
+                <img src=${this.thumbnail(board)} />
+                ${board.name} <span>${board.description}</span>
+              </a>
             `
         )}
       </nav>
@@ -47,6 +96,10 @@ class PageBoardList extends connect(store)(PageViewElement) {
 
   stateChanged(state) {
     this.boards = state.board.boards
+  }
+
+  thumbnail(board) {
+    return board.thumbnail || 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
   }
 }
 
